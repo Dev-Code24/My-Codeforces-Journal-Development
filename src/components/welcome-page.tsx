00d9f32af6c70da6dde01ac8041f00501b7a8784
Dev Code <hellodev_code@outlook.com>
2024-10-29 23:34:04 +0530

    fixed link direction

--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -3,20 +3,25 @@ const WelcomePage = () => {
     <div className="flex justify-center items-center flex-col">
       <div className="w-[355px]">
         <div className="w-full h-[200px] mb-4">
-          <a href="#">
+          <a href="#" target="_blank" rel="noopener noreferrer">
             <img src="/video.png" alt="" className="w-full h-full rounded-[14px]" />
           </a>
         </div>
 
         <p className="text-base px-1 text-center">
           How to use this extension{" "}
-          <a href="#" className="underline uppercase">
+          <a href="#" target="_blank" rel="noopener noreferrer" className="underline uppercase">
             Video Help
           </a>
         </p>
         <p className="text-base px-1 text-center">
           How to use this extension{" "}
-          <a href="https://github.com/Dev-Code24/My-Codeforces-Journal?tab=readme-ov-file#my-codeforces-journal" className="underline">
+          <a
+            href="https://github.com/Dev-Code24/My-Codeforces-Journal?tab=readme-ov-file#my-codeforces-journal"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
             Documentation
           </a>
         </p>
