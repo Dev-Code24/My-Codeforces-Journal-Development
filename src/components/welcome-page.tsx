bf54113775b340fea05f4b6d68cc270fd166d89f
Dev Code <hellodev_code@outlook.com>
2024-10-30 11:08:48 +0530

    update link url

--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -3,14 +3,14 @@ const WelcomePage = () => {
     <div className="flex justify-center items-center flex-col">
       <div className="w-[355px]">
         <div className="w-full h-[200px] mb-4">
-          <a href="#" target="_blank" rel="noopener noreferrer">
+          <a href="https://youtu.be/1vlqXxdEqFI" target="_blank" rel="noopener noreferrer">
             <img src="/video.png" alt="" className="w-full h-full rounded-[14px]" />
           </a>
         </div>
 
         <p className="text-base px-1 text-center">
           How to use this extension{" "}
-          <a href="#" target="_blank" rel="noopener noreferrer" className="underline uppercase">
+          <a href="https://youtu.be/1vlqXxdEqFI" target="_blank" rel="noopener noreferrer" className="underline uppercase">
             Video Help
           </a>
         </p>
